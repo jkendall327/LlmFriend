dd339e5e26f88ad41770704f8b7b6551ad09e2f5
jackkendall <jkendall3096@gmail.com>
2025-02-23 20:14:19 +0000

    Remove past code

--- LLMFriend/Models/ChatMessage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LLMFriend.Models
-{
-    public class ChatMessage
-    {
-        public string role { get; set; }
-        public string content { get; set; }
-    }
-}
--- LLMFriend/Program.cs
+++ LLMFriend/Program.cs
@@ -1,18 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.SemanticKernel.ChatCompletion;
-using LLMFriend;
+﻿using Microsoft.Extensions.Hosting;
 
 var host = Host.CreateApplicationBuilder(args);
-
-var customService = new CustomChatCompletionService
-{
-    ModelUrl = "http://localhost:1234/v1/chat/completions"
-};
-
-host.Services.AddSingleton<IChatCompletionService>(customService);
-
-host.Services.AddHostedService<LlmBackgroundService>();
 
 var app = host.Build();
 
